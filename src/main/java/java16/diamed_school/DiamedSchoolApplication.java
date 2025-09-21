17f4a5e791ed4126838c7760c9135c377d84d924
Aibek <clesh.meder.271@gmail.com>
2025-09-21 11:23:29 +0500

    Initial commit

---
+++ src/main/java/java16/diamed_school/DiamedSchoolApplication.java
@@ -0,0 +1,13 @@
+package java16.diamed_school;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiamedSchoolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiamedSchoolApplication.class, args);
+    }
+
+}
